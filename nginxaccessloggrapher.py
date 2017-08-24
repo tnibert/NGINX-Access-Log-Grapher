0711a160f8903b06867d343e120d555c4c62ac7f
Tim Nibert <docz2a@gmail.com>
2017-08-25 09:21:09 +1000

    fixed tally bug

--- nginxaccessloggrapher.py
+++ nginxaccessloggrapher.py
@@ -21,12 +21,12 @@ from matplotlib import dates
 
 IVAL=5                                                  # interval in minutes
 ROLL=5                                                  # how many intervals to roll
-IPSPLIT=True                                            # show graphs for individual IPs
-TOTALTRAFFIC=False                                       # show graph for total traffic (all IPs)
-IPVIEW=['10.100.22.101', '10.100.22.102']               # fill this list with the ips you want to check, if empty we will show all IPs
-FILENAME='bangkok-access.log'
+IPSPLIT=False                                            # show graphs for individual IPs
+TOTALTRAFFIC=True                                       # show graph for total traffic (all IPs)
+IPVIEW=[]               # fill this list with the ips you want to check, if empty we will show all IPs
+FILENAME='access.log'
 # graph types enabled:
-GRAPHNORMAL=False
+GRAPHNORMAL=True
 GRAPHROLLED=True
 GRAPHCUMULATIVE=True
 # add variables for a time range
@@ -76,25 +76,35 @@ def convertStrToDatetime(dtstr):
     return datetime.datetime.strptime(dtstr, "%d/%b/%Y:%H:%M:%S")
 
 def generate_graph_dict(times):
+    """
+    param list of times, returned from process_log in this file
+    returns ordered dictionary of form { time_slice_start : request_count }
+    """
+    #block is the amount of time that we tally over, set with IVAL global
     block = timedelta(minutes=IVAL)
     start = times[0]
     graphdict = OrderedDict()
-    #print start
+    end = start + block
+
+    # iterate through all times and tally up each request, if we reach end, move to next block
     for time in times:
-        end = start + block
-        if(time < end):
+        if start not in graphdict:
+            graphdict[start] = 0
+        if(time <= end):
             try:
                 graphdict[start] += 1
             except:
                 graphdict[start] = 1
         else:
             # if we haven't yet created a count for the time (eg if no entries in time range) set to 0, then seek start to next block
-            if start not in graphdict:
+            while(time > end):
+                start = end
+                end = start + block
                 graphdict[start] = 0
-            start = end
-            #print start
+            graphdict[start] += 1
 
     return graphdict
+
 
 def graphcumulative(graphdict, ip='full system'):
     dates = graphdict.keys()
@@ -178,22 +188,28 @@ if __name__ == '__main__':
 
     # return tuple of times and ips
     timesandips = process_log(log_file)
+    print len(timesandips)
 
     if len(IPVIEW) == 0:
         ips = [x[0] for x in timesandips]
         uniqueips = list(set(ips))
         print uniqueips
+        #try:
+        #    input = raw_input
+        #except NameError:
+        #    pass
     else:
         uniqueips = IPVIEW
 
     # create graphs of total traffic
     if(TOTALTRAFFIC):
         times = [x[1] for x in timesandips]
-
+        print len(times)
         gd = generate_graph_dict(times)
-        #for k,v in gd.items():
-        #    if(v > 0):
-        #        print str(k) + " " + str(v)
+        # display time intervals and number present
+        for k,v in gd.items():
+            if(True):
+                print str(k) + " " + str(v)
                 #pass
         ivalstr = "On interval " + str(IVAL) + " minutes for full system"
         if(GRAPHNORMAL): graph(gd, ivalstr)
@@ -206,9 +222,9 @@ if __name__ == '__main__':
             times = [x[1] for x in timesandips if x[0] == ip]
 
             gd = generate_graph_dict(times)
-            #for k,v in gd.items():
-            #    if(v > 0):
-            #        print str(k) + " " + str(v)
+            for k,v in gd.items():
+                if(v > 0):
+                    print str(k) + " " + str(v)
                     #pass
             ivalstr = "On interval " + str(IVAL) + " minutes for " + ip
             if(GRAPHNORMAL): graph(gd, ivalstr)
